e58d6e06ff76e955e0003fc8c8466e8585fcbb6f
Juan Pablo Lopez <38148731+megagringa@users.noreply.github.com>
2023-02-03 23:08:12 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask
+from flask import render_template
+
+app=Flask(__name__)
+
+@app.route('/')
+def inicio():
+    return render_template('sitio/index.html')
+
+@app.route('/libros')
+def libros():
+    return render_template('sitio/libros.html')
+
+@app.route('/nosotros')
+def nosotros():
+    return render_template('sitio/nosotros.html')
+
+@app.route('/admin')
+def admin_index():
+    return render_template('admin/index.html') 
+@app.route('/admin/login')
+def admin_login():
+    return render_template('admin/login.html')       
+
+if __name__=='__main__':
+    app.run(debug=True)
